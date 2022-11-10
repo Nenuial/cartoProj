8714592391da935c53a2fa9240897cf666ed8afe
Pascal Burkhard <pascal.burkhard@gmail.com>
2022-11-10 21:45:48 +0100

    Update carte.R to population map

--- carte.R
+++ carte.R
@@ -4,7 +4,19 @@ library(tidyverse)
 cantons <- read_sf("geodata/Cantons.shp")
 lacs <- read_sf("geodata/Lacs.shp")
 
+population <- read_csv("statdata/swiss_pop_2021.csv")
+
 cantons %>% 
-  ggplot() +
+  left_join(population, by = c("KTNR" = "ID")) -> donnees_carte
+
+donnees_carte %>% 
+  ggplot(aes(fill = Population)) + 
   geom_sf() +
-  geom_sf(data = lacs, fill = "blue")
+  geom_sf(data = lacs, fill = "#adf9ff") +
+  coord_sf(datum = NA) +
+  scale_fill_viridis_c(direction = -1) +
+  theme_minimal() +
+  labs(
+    title = "Population de la Suisse",
+    subtitle = "en 2021"
+  )
