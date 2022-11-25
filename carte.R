00b59ecab68a83a75cecefab93448c7c5dbe8c30
Pascal Burkhard <pascal.burkhard@gmail.com>
2022-11-25 08:07:07 +0100

    Update to divorce map

--- carte.R
+++ carte.R
@@ -5,18 +5,22 @@ cantons <- read_sf("geodata/Cantons.shp")
 lacs <- read_sf("geodata/Lacs.shp")
 
 population <- read_csv("statdata/swiss_pop_2021.csv")
+divorces <- read_csv("statdata/swiss_divorces_2021.csv")
 
 cantons %>% 
-  left_join(population, by = c("KTNR" = "ID")) -> donnees_carte
+  left_join(population, by = c("KTNR" = "ID")) %>% 
+  left_join(divorces, by = c("KTNR" = "ID"))  %>% 
+  mutate(tx_divorces = Divorces / Population * 1000) -> donnees_carte
 
 donnees_carte %>% 
-  ggplot(aes(fill = Population)) + 
-  geom_sf() +
-  geom_sf(data = lacs, fill = "#adf9ff") +
+  ggplot(aes(fill = tx_divorces)) + 
+  geom_sf(size = .2) +
+  geom_sf(size = .2, data = lacs, fill = "#adf9ff") +
   coord_sf(datum = NA) +
-  scale_fill_viridis_c(direction = -1) +
+  scale_fill_viridis_c(option = "B", direction = -1) +
   theme_minimal() +
   labs(
-    title = "Population de la Suisse",
-    subtitle = "en 2021"
+    title = "Divorces en Suisse",
+    subtitle = "en 2021, pour 1000 habitants",
+    fill = "Taux de divorce"
   )
