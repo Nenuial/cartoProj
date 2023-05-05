d848fdea6c502e64062e334278a65ab46cf60140
Pascal Burkhard <pascal.burkhard@gmail.com>
2023-05-05 09:40:10 +0200

    Update Binder

--- monde.R
+++ monde.R
@@ -9,7 +9,7 @@ ne_countries(returnclass = "sf") %>%
   left_join(donnees_carte, by = c("adm0_a3" = "iso3c")) %>% 
   ggplot(aes(fill = `SH.IMM.MEAS`)) +
   geom_sf() +
-  coord_sf(crs = "+proj=eqearth", datum = NA) +
+  coord_sf(crs = "+proj=eck4", datum = NA) +
   scale_fill_viridis_b(direction = -1) +
   theme_minimal() +
   labs(fill = "Taux de vaccination")
